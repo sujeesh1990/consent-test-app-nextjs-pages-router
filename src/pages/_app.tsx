afff62a29f092e3e31d9ef345012f3e2af5565dc
Sujeesh Sidharthan <sujeesh1990@gmail.com>
2025-11-17 11:29:45 +0530

    Added PostHog tracking back in

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,33 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
+      useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      defaults: '2025-05-24',
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      }
+    })
+  }, [])
   return (
     <>
-      <Component {...pageProps} />
-
       <Script
         id="relyance-consent-agent"
-        src="https://consent.app.relyancestage.xyz/relyance-agent.js"
-        data-relyance-consent-appId="app-8b88aca7-13d2-528c-99de-ddcbd55dde01"
-        data-relyance-zero-fire-mode="true"
+        src={process.env.NEXT_PUBLIC_RELYANE_SRC}
+        data-relyance-consent-appId={process.env.NEXT_PUBLIC_RELYANCE_APPID}
+        data-relyance-zero-fire-mode={process.env.NEXT_PUBLIC_RELYANCE_ZERO_FIRE_MODE}
         strategy="afterInteractive"
       />
+      <PostHogProvider client={posthog}>
+        <Component {...pageProps} />
+      </PostHogProvider>
+      <Component {...pageProps} />
     </>
   );
 }
