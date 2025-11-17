5bc85219785a2c10a2c386b31b11b292ba622cb1
Sujeesh Sidharthan <sujeesh1990@gmail.com>
2025-11-17 11:36:38 +0530

    Fixed env variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Script
         id="relyance-consent-agent"
-        src={process.env.NEXT_PUBLIC_RELYANE_SRC}
+        src={process.env.NEXT_PUBLIC_RELYANCE_SRC}
         data-relyance-consent-appId={process.env.NEXT_PUBLIC_RELYANCE_APPID}
         data-relyance-zero-fire-mode={process.env.NEXT_PUBLIC_RELYANCE_ZERO_FIRE_MODE}
         strategy="afterInteractive"
