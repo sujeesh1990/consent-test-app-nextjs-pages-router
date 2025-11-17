d356cf1e156caf7ce956cef4c291616dd8e60e23
Sujeesh Sidharthan <sujeesh.sidharthan@relyance.ai>
2025-11-17 08:59:06 +0530

    Updted Agent to staging tenant

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <Script
+        id="relyance-consent-agent"
+        src="https://consent.app.relyancestage.xyz/relyance-agent.js"
+        data-relyance-consent-appId="app-8b88aca7-13d2-528c-99de-ddcbd55dde01"
+        data-relyance-zero-fire-mode="true"
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
